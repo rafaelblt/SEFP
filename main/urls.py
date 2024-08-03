6f4701c6bfcb6fddf2c44948061564ebab73d1d5
rafaelblt <rafinha.b.lucena@gmail.com>
2024-08-03 12:09:58 -0300

    news views & forms

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import AlunoCreateView, AlunoUpdateView
 
 urlpatterns = [
     path('',views.alunoView, name='aluno-lista'),
-    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+    path('aluno/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+    path('aluno/create/', AlunoCreateView.as_view(), name='aluno-create'),
+    path('aluno/<int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-update'),
+    path('aluno/<int:id>/delete/', views.deleteAluno, name='delete-aluno')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
 def alunoView(request):
     alunos_list = Aluno.objects.all()
@@ -10,3 +11,55 @@ def alunoIDview(request, id):
     aluno = get_object_or_404(Aluno, pk=id)
     print(aluno)
     return render(request, 'main/alunoID.html', {'aluno': aluno})
+
+"""
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print('name:', name)
+        print('Email:', email)
+        print('Message:', message)
+    return render(request, 'main/contact.html')
+"""
+    
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import AlunoForm
+
+# AS FORMAS DE CLASSE FORAM UTILIZADAS PARA FINS DIDÁTICOS, PODER SER POR FUNÇÃO OU CLASSE 
+
+class AlunoCreateView(CreateView):
+    model = Aluno
+    form_class = AlunoForm
+    success_url = reverse_lazy('aluno-lista')
+    template_name = 'main/aluno_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+def aluno_create_view(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            aluno = form.save(commit=False)
+            aluno.user = request.user
+            aluno.save()
+            return redirect(reverse('aluno-lista'))
+    else:
+        form = AlunoForm()
+
+    return render(request, 'aluno_form.html', {'form': form})
+
+class AlunoUpdateView(UpdateView):
+    model = Aluno
+    form_class = AlunoForm
+    template_name = 'main/aluno_form.html'
+    success_url = reverse_lazy('aluno-lista')
+
+def deleteAluno(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    aluno.delete()
+    return redirect('/')
